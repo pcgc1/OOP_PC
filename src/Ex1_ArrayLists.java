24e80f7e4262ae650700e2713e112f5f7f4e1c30
pcgc1 <chalutphilip@gmail.com>
2024-11-05 14:55:08 -0700

    bruh

---
+++ src/Ex1_ArrayLists.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Ex1_ArrayLists {
+
+    public static void run(){
+        ArrayList <String> allNames = new ArrayList();
+//        List <String> allNames = new ArrayList();
+
+        ArrayList <Double> allDollars = new ArrayList();
+        ArrayList <Integer> allNums = new ArrayList();
+        ArrayList <Boolean> allOscars = new ArrayList();
+    }
+
+}//class
