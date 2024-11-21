84d4a884e3cf4d5675db3bccd66cf1224088295e
pcgc1 <chalutphilip@gmail.com>
2024-11-21 15:23:22 -0700

    bruh

--- src/Assignments/As1_Main.java
+++ src/Assignments/As1_Main.java
@@ -14,6 +14,18 @@ public class As1_Main {
         allCrops.add( new As1_Crop("Lentils", 22.4, "bushels per acre", 0.28) );
         allCrops.add( new As1_Crop("Oats", 90.9, "bushels per acre", 3.68) );
 
+        //Setting acres
+        allCrops.get(0).setAcres(485);
+        allCrops.get(1).setAcres(75);
+        allCrops.get(2).setAcres(150);
+        allCrops.get(3).setAcres(250);
+        allCrops.get(4).setAcres(40);
+
+        //print crops
+        for (int i = 0; i < allCrops.size(); i++) {
+            System.out.println(allCrops.get(i).toString());
+        }
+
     }//run
 
 }//class
