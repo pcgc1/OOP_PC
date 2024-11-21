07b1686a1192f38e266816da9b41271f7d13492f
pcgc1 <chalutphilip@gmail.com>
2024-11-21 15:03:56 -0700

    bruh

--- src/Assignments/As1_Crop.java
+++ src/Assignments/As1_Crop.java
@@ -10,15 +10,23 @@ public class As1_Crop {
 
 
     //constructor method
-    public As1_Crop(String n, double y, String u, double p, int a){
+    public As1_Crop(String n, double y, String u, double p){
         name = n;
         yield = y;
         units = u;
         price = p;
-        acres = a;
+        acres = 0;
 
     }//constructor
 
+    public String toString(){
+        return name + " " + yield + " " + units + " " + price + " " + acres;
+    }//toString
+
+
+    public void setAcres(int acres) {
+        this.acres = acres;
+    }//set acres
 
 
 }//class
--- src/Assignments/As1_Main.java
+++ src/Assignments/As1_Main.java
@@ -7,6 +7,12 @@ public class As1_Main {
     public static void run(){
         ArrayList<As1_Crop> allCrops = new ArrayList<>();
 
+        //adding crops to arraylist
+        allCrops.add( new As1_Crop("Sugar beets", 34.9, "tonnes per acre", 38.77) );
+        allCrops.add( new As1_Crop("Canola", 38.1, "bushels per acre", 13.95) );
+        allCrops.add( new As1_Crop("Barley", 75.9, "bushels per acre", 5.25) );
+        allCrops.add( new As1_Crop("Lentils", 22.4, "bushels per acre", 0.28) );
+        allCrops.add( new As1_Crop("Oats", 90.9, "bushels per acre", 3.68) );
 
     }//run
 
