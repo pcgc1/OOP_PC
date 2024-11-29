40372b45ad73f6a7987137e6ce694f6853a66961
pcgc1 <chalutphilip@gmail.com>
2024-11-29 15:25:14 -0700

    bruh

--- src/Assignments/As2_Team.java
+++ src/Assignments/As2_Team.java
@@ -5,7 +5,10 @@ public class As2_Team {
     private String nickname;
     private String city;
     private String league;
-    
+    private double moneySpent;
+    private int currentWins;
+    private int currentLosses;
+
 
 
 }//class
