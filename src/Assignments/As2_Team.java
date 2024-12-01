fea91e7e1b7cdd44d74a8bc8084b64f8958bb4ff
pcgc1 <chalutphilip@gmail.com>
2024-12-01 15:48:00 -0700

    b

--- src/Assignments/As2_Team.java
+++ src/Assignments/As2_Team.java
@@ -6,9 +6,19 @@ public class As2_Team {
     private String city;
     private String league;
     private double moneySpent;
-    private int currentWins;
-    private int currentLosses;
+    private int totalWins;
+    private int totalLosses;
 
+
+    public As2_Team(String n, String c, String l, double m, int win, int loss){
+        nickname = n;
+        city = c;
+        league = l;
+        moneySpent = m;
+        totalWins = win;
+        totalLosses = loss;
+
+    }//constructor
 
 
 }//class
